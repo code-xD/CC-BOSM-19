94f052a909eb0b16a10043b3bc3d98a8740644ef
Aarjav Jain <aarjav00@gmail.com>
2019-08-30 00:59:29 +0530

    KeyErrors Handled

--- treasure/views.py
+++ treasure/views.py
@@ -45,12 +45,12 @@ def login(request):
             try:
                 participant = Participant.objects.get(email=email)
                 if participant.team:
-                    return JsonResponse({"message": "User Login Successful!", "status": 1, "participant id": participant.id})
+                    return JsonResponse({"message": "User Login Successful!", "status": 2, "participant id": participant.unique_id})
                 else:
-                    return JsonResponse({"message": "Participant does not belong to any Team!", "status": 0})
+                    return JsonResponse({"message": "Participant does not belong to any Team!", "status": 1, "participant id": participant.unique_id})
             except Participant.DoesNotExist:
                 participant = Participant.objects.create(email=email)
-                return JsonResponse({"message": "Participant does not belong to any Team!", "status": 0})
+                return JsonResponse({"message": "Participant does not belong to any Team!", "status": 1, "participant id": participant.unique_id})
 
         except ValueError:
             # Invalid token
@@ -73,8 +73,21 @@ def team_register(request):
         except Exception:
             return JsonResponse({"message": "Please check syntax of JSON data passed.", "status": 0})
 
-        team_name = data['team_name']
-        participants = data['participant_list']
+        try:
+            team_name = data['team_name']
+            participants = data['participant_list']
+            part_id = data['participant_id']
+        except KeyError as missing_data:
+            return JsonResponse({'message': 'Field missing: {0}'.format(missing_data), 'status': 0})
+
+        try:
+            participant1 = Participant.objects.get(unique_id=part_id)
+        except Participant.DoesNotExist:
+            return JsonResponse({"message": "Please login first.", "status": 0})
+
+        if participant1.team:
+            return JsonResponse({"message": "Your Team is already Registered.", "status": 0})
+
         try:
             with transaction.atomic():
                 team = Team.objects.create(name=team_name, state=0)
@@ -84,7 +97,7 @@ def team_register(request):
                         raise Exception
                     participant.team = team
                     participant.save()
-            return JsonResponse({"message": "Team registered successfully", "status": 1, "team_id": team.id})
+            return JsonResponse({"message": "Team registered successfully", "status": 1, "team_name": team.name})
         except Exception:
             return JsonResponse({"message": "Participant team already exists.", "status": 0})
 
@@ -104,7 +117,10 @@ def question_details(request):
             except Exception:
                 return JsonResponse({"message": "Please check syntax of JSON data passed.", "status": 0})
 
-            part_id = data['participant_id']
+            try:
+                part_id = data['participant_id']
+            except KeyError as missing_data:
+                return JsonResponse({'message': 'Field missing: {0}'.format(missing_data), 'status': 0})
 
             try:
                 with transaction.atomic():
@@ -130,21 +146,24 @@ def check_question_answer(request):
             data = json.loads(request.body.decode("utf-8").replace("'",'"'))
         except Exception:
             return JsonResponse({"message": "Please check syntax of JSON data passed.", "status": 0})
+        try:
+            question_id = data['question_id']
+            participant_id = data['participant_id']
+            ans_saved = data['ans']  #there is no answer in the database yet.
+        except KeyError as missing_data:
+            return JsonResponse({'message': 'Field missing: {0}'.format(missing_data), 'status': 0})
 
-        question_id = data['question_id']
-        participant_id = data['participant_id']
         try:
             with transaction.atomic():
                 question = Question.objects.get(unique_id=question_id)
-                ans_saved = data['ans']  #there is no answer in the database yet.
                 answer = Answer.objects.get(question=question)
-                answer.times_answered += 1
-                answer.save()
                 participant = Participant.objects.get(unique_id=participant_id)
                 team = participant.team
             if ans_saved == answer.answer:
                 team.state += 1
                 team.save()
+                answer.times_answered += 1
+                answer.save()
                 return JsonResponse({"message":"Team answered the question correctly.", "status": 1})
             else:
                 return JsonResponse({"message":"Answer is incorrect", "status": 0})
